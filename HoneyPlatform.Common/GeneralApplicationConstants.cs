bce4749fd79ea80a233289c45f26bbe332532c97
Miro <miroslav_djutev@mail.bg>
2023-07-31 20:40:55 +0300

    Adding the web core and an infrastructure project with future extension tools like: decimal model minder, claims principal extensions, view models extensions, webapp builder extensions.

---
+++ HoneyPlatform.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace HoneyPlatform.Common
+{
+    public static class GeneralApplicationConstants
+    {
+        public const int ReleaseYear = 2023;
+
+        public const int DefaultPage = 1;
+        public const int EntitiesPerPage = 3;
+
+        public const string AdminRoleName = "Administrator";
+        public const string DevelopmentAdminEmail = "admin@honeyplatform.bg";
+    }
+}
---
+++ HoneyPlatform.Web/Controllers/UserController.cs
@@ -0,0 +1,103 @@
+﻿namespace HoneyPlatform.Web.Controllers
+{
+    using Griesoft.AspNetCore.ReCaptcha;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Data.Models;
+    using ViewModels.User;
+
+    using static Common.NotificationMessagesConstants;
+
+    public class UserController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserController(SignInManager<ApplicationUser> signInManager,
+                              UserManager<ApplicationUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateRecaptcha(Action = nameof(Register),
+            ValidationFailedAction = ValidationFailedAction.ContinueRequest)]
+        public async Task<IActionResult> Register(RegisterFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            await userManager.SetEmailAsync(user, model.Email);
+            await userManager.SetUserNameAsync(user, model.Email);
+
+            IdentityResult result = 
+                await userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+
+            await signInManager.SignInAsync(user, false);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string? returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            LoginFormModel model = new LoginFormModel()
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = 
+                await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+            if (!result.Succeeded)
+            {
+                TempData[ErrorMessage] =
+                    "There was an error while logging you in! Please try again later or contact an administrator.";
+
+                return View(model);
+            }
+
+            return Redirect(model.ReturnUrl ?? "/Home/Index");
+        }
+    }
+}
