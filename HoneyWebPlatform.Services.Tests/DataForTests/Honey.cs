6a2396fea28e5642234ce4804bfeb6f7b0e996ee
Miro <miroslav_djutev@mail.bg>
2023-08-05 23:30:40 +0300

    Trying some Unit stuff from Kenov

---
+++ HoneyWebPlatform.Services.Tests/DataForTests/Honey.cs
@@ -0,0 +1,24 @@
+﻿namespace HoneyWebPlatform.Services.Tests.DataForTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using HoneyWebPlatform.Data.Models;
+
+    public static class Honeys
+    {
+        public static IEnumerable<Honey> TenPublicHoneys
+            => Enumerable.Range(0, 10).Select(i => new Honey
+            {
+                Id = default,
+                Title = null,
+                Origin = null,
+                Description = null,
+                ImageUrl = null,
+                Price = 6,
+                NetWeight = 100,
+                CreatedOn = default,
+                YearMade = 0,
+                IsActive = false,
+            });
+    }
+}
---
+++ HoneyWebPlatform.Services.XUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HoneyWebPlatform.Services.XUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
